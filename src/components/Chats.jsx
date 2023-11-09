b2c53cf8915cb4bf34dbd35e9cf8bd3d5b56798b
Manikandan1818 <simbumanii18@gmail.com>
2023-11-09 21:25:47 +0530

    chats & search components completed!

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import Mani from "../assets/mani.jpg";
 
 const Chats = () => {
-  return <div>Chats</div>;
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img src={Mani} alt="" />
+        <div className="userChatInfo">
+          <span>Manikandan</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src={Mani} alt="" />
+        <div className="userChatInfo">
+          <span>Manikandan</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src={Mani} alt="" />
+        <div className="userChatInfo">
+          <span>Manikandan</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src={Mani} alt="" />
+        <div className="userChatInfo">
+          <span>Manikandan</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src={Mani} alt="" />
+        <div className="userChatInfo">
+          <span>Manikandan</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src={Mani} alt="" />
+        <div className="userChatInfo">
+          <span>Manikandan</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src={Mani} alt="" />
+        <div className="userChatInfo">
+          <span>Manikandan</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,7 +5,7 @@ const Search = () => {
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" placeholder="" />
+        <input type="text" placeholder="Find a user" />
       </div>
       <div className="userChat">
         <img src={Mani} alt="" />
