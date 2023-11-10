3e3239e340f001c55b8a1fb0277e863114577985
Manikandan1818 <simbumanii18@gmail.com>
2023-11-10 20:17:48 +0530

    message component working on

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import SendIcon from "@mui/icons-material/Send";
 
 const Input = () => {
-  return <div className="input">Input</div>;
+  return (
+    <div className="input">
+      <input type="text" placeholder="Type Something..." />
+      <div className="send">
+        <AttachFileIcon />
+        <input type="file" style={{ display: "none" }} id="file" />
+        <label htmlFor="file">
+          <AddPhotoAlternateIcon />
+        </label>
+        <button>
+          <SendIcon />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Input;
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Mani from "../assets/mani.jpg";
 
 const Message = () => {
-  return <div className="message">Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img src={Mani} alt="" />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello, Mani</p>
+        <img src={Mani} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
