d00929bb98a5d4e8dde6ebcd815464e6ab3e42fc
Manikandan1818 <simbumanii18@gmail.com>
2023-11-14 14:36:31 +0530

    firebase insalled

---
+++ src/components/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhLbvHvR4jk8YlPtiaGV4HciztZx1HEF8",
+  authDomain: "clarionchat-3428b.firebaseapp.com",
+  projectId: "clarionchat-3428b",
+  storageBucket: "clarionchat-3428b.appspot.com",
+  messagingSenderId: "1044841619604",
+  appId: "1:1044841619604:web:c5cc7acd6990b7fe7e9481",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
